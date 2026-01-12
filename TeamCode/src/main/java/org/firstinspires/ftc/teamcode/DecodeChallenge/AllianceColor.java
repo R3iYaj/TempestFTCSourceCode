a54520a189aff74b3d319e43524e10eb9ce0beff
R3iYaj <rei.huabci.yang@gmail.com>
2026-01-12 07:46:18 -0600

    feat: add camera controller to scan april tags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/AllianceColor.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.DecodeChallenge;
+
+public enum AllianceColor {
+    Red, Blue
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/AprilTagConstant.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.DecodeChallenge;
+
+public class AprilTagConstant {
+    public static final int BlueGoal = 20;
+    public static final int GPP = 21;
+    public static final int PGP = 22;
+    public static final int PPG = 23;
+    public static final int RedGoal = 24;
+}
