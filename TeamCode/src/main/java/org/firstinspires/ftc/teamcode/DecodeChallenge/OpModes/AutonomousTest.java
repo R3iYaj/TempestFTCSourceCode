6bb353c49005d652725c858847d453561fb00e12
R3iYaj <rei.huabci.yang@gmail.com>
2026-01-12 07:46:43 -0600

    feat: add pathing class and state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/OpModes/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/OpModes/AutonomousTest.java
@@ -5,68 +5,112 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.DecodeChallenge.AllianceColor;
 import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.RobotMapping;
 import org.firstinspires.ftc.teamcode.DecodeChallenge.PedroPathing.Constants;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.DecodePathing;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.FireSequence;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.DecodeDriveSystemStateMachine;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.FireSequenceSystemStateMachine;
 
-@Autonomous(name="Autonomous Pedro Test", group="Test")
+@Autonomous(name="Autonomous Test", group="Test")
 public class AutonomousTest extends LinearOpMode {
-    private enum RobotState { Preloaded, Firing, Move, Idle }
+    private enum RobotState { Preloaded, Firing, MoveToFirstLine, MoveToSecondLine, MoveToThirdLine, Complete }
 
     private RobotMapping _robotMapping;
-    private FireSequence _fireSequence;
+    private FireSequenceSystemStateMachine _fireSequenceSystem;
     private Follower _follower;
-    private DecodePathing _pathing;
+    private DecodeDriveSystemStateMachine _pathing;
+
+    private final ElapsedTime _stateTimer = new ElapsedTime();
     private RobotState _currentAutoState = RobotState.Preloaded;
+
+    private final AllianceColor allianceColor = AllianceColor.Blue;
 
     @Override
     public void runOpMode() {
 
         _robotMapping = new RobotMapping(hardwareMap);
         _follower = Constants.createFollower(hardwareMap);
-        _pathing = new DecodePathing(_follower);
-        _fireSequence = new FireSequence(telemetry, _robotMapping);
 
-        telemetry.addData("OpMode", "Autonomous PEDRO TEST");
+        _pathing = new DecodeDriveSystemStateMachine(telemetry, _follower, _robotMapping, allianceColor);
+        _fireSequenceSystem = new FireSequenceSystemStateMachine(telemetry, _robotMapping);
+
+        telemetry.addData("OpMode", "Autonomous TEST");
         telemetry.update();
 
         waitForStart();
 
-        _fireSequence.InitFireMode();
+        FireSequenceSystemStateMachine.LaunchState launchState;
 
         while (opModeIsActive()) {
 
             _follower.update();
 
-            FireSequence.LaunchState launchState = _fireSequence.GetStatus();
-
-            switch (_currentAutoState){
-                case Preloaded:
-                    if (launchState == FireSequence.LaunchState.ReadyToFire){
-                        _fireSequence.Fire();
-                    }
-
-                    if (launchState == FireSequence.LaunchState.Off){
-                        _currentAutoState = RobotState.Move;
-                    }
-                    break;
-
-                case Move:
-                    if (!_follower.isBusy()){
-                        _currentAutoState = RobotState.Idle;
-                    }
-                    break;
-
-                case Idle:
-                    // Do nothing
-                    break;
-            }
+            launchState = _fireSequenceSystem.GetStatus();
 
             telemetry.addData("Auto State", _currentAutoState);
             telemetry.addData("Fire State", launchState);
             telemetry.addData("Pos X", _follower.getPose().getX());
             telemetry.addData("Pos Y", _follower.getPose().getY());
+
+            switch (_currentAutoState){
+                case Preloaded:
+                    _fireSequenceSystem.InitFireMode();
+                    _currentAutoState = RobotState.Firing;
+                    break;
+
+                case Firing:
+                    if (launchState == FireSequenceSystemStateMachine.LaunchState.ReadyToFire){
+                        _fireSequenceSystem.Fire();
+                    }
+
+                    if (launchState == FireSequenceSystemStateMachine.LaunchState.Off){
+                        switch (_pathing.IdentifyNextSpecimen())
+                        {
+                            case 1:
+                                _currentAutoState = RobotState.MoveToFirstLine;
+                                _pathing.GoToFirstRow();
+                                break;
+
+                            case 2:
+                                _currentAutoState = RobotState.MoveToSecondLine;
+                                break;
+
+                            case 3:
+                                _currentAutoState = RobotState.MoveToThirdLine;
+                                break;
+
+                            default:
+                                _currentAutoState = RobotState.Complete;
+                        }
+                    }
+                    break;
+
+                case MoveToFirstLine:
+
+
+
+                    if (!_follower.isBusy()){
+                        _currentAutoState = RobotState.MoveToSecondLine;
+                    }
+                    break;
+
+                case MoveToSecondLine:
+                    if (!_follower.isBusy()){
+                        _currentAutoState = RobotState.MoveToThirdLine;
+                    }
+                    break;
+
+                case MoveToThirdLine:
+                    if (!_follower.isBusy()){
+                        _currentAutoState = RobotState.Complete;
+                    }
+                    break;
+
+                case Complete:
+                    // Do nothing
+                    break;
+            }
+
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/Systems/DecodeDriveSystemStateMachine.java
@@ -0,0 +1,341 @@
+package org.firstinspires.ftc.teamcode.DecodeChallenge.Systems;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.AllianceColor;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.AprilTagConstant;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.Controllers.CameraController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecodeDriveSystemStateMachine {
+
+    public enum DriveState { DecdingNextSpeciment, DriveToRow, WaitForLoadTrigger, LoadingSpecimens, LoadingComplete, DriveToLaunch, ReadyToFire, Idle }
+
+    private final Telemetry _telemetry;
+    private final Follower _follower;
+    private final CameraController _camera;
+
+    private PathChain _pathMoveToFirstRow;
+    private PathChain _pathApproachFirstRowStack;
+    private PathChain _pathReturnFirstRowToLaunch;
+
+    private PathChain _pathMoveToSecondRow;
+    private PathChain _pathApproachSecondRowStack;
+    private PathChain _pathReturnSecondRowToLaunch;
+
+    private PathChain _pathMoveToThirdRow;
+    private PathChain _pathApproachThirdRowStack;
+    private PathChain _pathReturnThirdRowToLaunch;
+
+    private final AllianceColor _allianceColor;
+
+    private List<Integer> _completedSpecimens = new ArrayList<>();
+    private int _targetSpecimenId;
+    private int _startingSpecimen;
+    private DriveState _currentDriveState;
+    private boolean _loadTriggerPulled;
+    private boolean _returnTriggerPulled;
+
+    public DecodeDriveSystemStateMachine(Telemetry telemetry, Follower follower, RobotMapping rm, AllianceColor allianceColor) {
+        _telemetry = telemetry;
+        _follower = follower;
+        _allianceColor = allianceColor;
+        _camera = new CameraController(telemetry, rm.Webcam, allianceColor);
+
+        _loadTriggerPulled = false;
+        _returnTriggerPulled = false;
+    }
+
+    public void Init(){
+
+        int startingSpecimen = _camera.GetDecode();
+        if (startingSpecimen != -1){
+            _startingSpecimen = startingSpecimen;
+        }
+
+        Pose startPosition = new Pose(56.000, 8.000, Math.toRadians(90));
+        Pose cameraPosition = _camera.GetRobotPose();
+
+        if (cameraPosition != null) {
+            _follower.setStartingPose(cameraPosition);
+        }
+        else {
+            _follower.setStartingPose(startPosition);
+        }
+
+        BuildPaths();
+    }
+
+    public void ScanNextSpecimen(){
+        if (_currentDriveState == DriveState.ReadyToFire){
+            MarkSpecimentComplete(_targetSpecimenId);
+        }
+
+        ChangeState(DriveState.DecdingNextSpeciment);
+    }
+    public void StartLoading(){
+        _loadTriggerPulled = true;
+    }
+
+    public void ReturnToLaunch(){
+        _returnTriggerPulled = true;
+    }
+
+    public boolean IsWaitingToLoad(){
+        return _currentDriveState == DriveState.WaitForLoadTrigger;
+    }
+
+    public boolean LoadComplete(){
+        return _currentDriveState == DriveState.LoadingComplete;
+    }
+
+    private void BuildPaths(){
+
+        _pathMoveToFirstRow = _follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(56.000, 8.000),
+                                new Pose(62.814, 32.989),
+                                new Pose(40.432, 35.385)))
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
+                .build();
+
+        _pathApproachFirstRowStack = _follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(40.432, 35.385),
+                                new Pose(15.417, 35.417)))
+                .setTangentHeadingInterpolation()
+                .build();
+
+        _pathReturnFirstRowToLaunch = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR SECOND ROW
+        _pathMoveToSecondRow = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR SECOND ROW
+        _pathApproachSecondRowStack = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR SECOND ROW
+        _pathReturnSecondRowToLaunch = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR THIRD ROW
+        _pathMoveToThirdRow = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR THIRD ROW
+        _pathApproachThirdRowStack = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+
+        // TODO: NEED COORDINATES UPDATED FOR THIRD ROW
+        _pathReturnThirdRowToLaunch = _follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(15.417, 35.417),
+                                new Pose(53.737, 35.704),
+                                new Pose(56.000, 8.000)))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+    }
+
+    private void GoToFirstRow(){
+        _follower.followPath(_pathMoveToFirstRow);
+    }
+
+    private void ApproachFirstRowForLoading(){
+        _follower.followPath(_pathApproachFirstRowStack);
+    }
+
+    private void ReturnFirstRowToLaunch(){
+        _follower.followPath(_pathReturnFirstRowToLaunch);
+    }
+
+    private void GoToSecondRow(){
+        _follower.followPath(_pathMoveToSecondRow);
+    }
+
+    private void ApproachSecondRowForLoading(){
+        _follower.followPath(_pathApproachSecondRowStack);
+    }
+
+    private void ReturnSecondRowToLaunch(){
+        _follower.followPath(_pathReturnSecondRowToLaunch);
+    }
+
+    private void GoToThirdRow(){
+        _follower.followPath(_pathMoveToThirdRow);
+    }
+
+    private void ApproachThirdRowForLoading(){
+        _follower.followPath(_pathApproachThirdRowStack);
+    }
+
+    private void ReturnThirdRowToLaunch(){
+        _follower.followPath(_pathReturnThirdRowToLaunch);
+    }
+
+    private int IdentifyNextSpecimen(){
+
+        if (!_completedSpecimens.contains(_startingSpecimen)) {
+            return _startingSpecimen;
+        }
+
+        if (!_completedSpecimens.contains(AprilTagConstant.GPP)){
+            return AprilTagConstant.GPP;
+        }
+
+        if (!_completedSpecimens.contains(AprilTagConstant.PGP)){
+            return AprilTagConstant.PGP;
+        }
+
+        if (!_completedSpecimens.contains(AprilTagConstant.PPG)){
+            return AprilTagConstant.PPG;
+        }
+
+        return -1;
+    }
+
+    private void MarkSpecimentComplete(int specimentId){
+        _completedSpecimens.add(specimentId);
+    }
+
+    private void ChangeState(DriveState newState){
+        _currentDriveState = newState;
+    }
+
+    public DriveState GetUpdate(){
+
+        switch (_currentDriveState){
+
+            case DecdingNextSpeciment:
+                _targetSpecimenId = IdentifyNextSpecimen();
+                switch (_targetSpecimenId){
+
+                    case AprilTagConstant.GPP:
+                        GoToFirstRow();
+                        break;
+
+                    case AprilTagConstant.PGP:
+                        GoToSecondRow();
+                        break;
+
+                    case AprilTagConstant.PPG:
+                        GoToThirdRow();
+                        break;
+
+                    default:
+                        ChangeState(DriveState.Idle);
+                }
+
+                ChangeState(DriveState.DriveToRow);
+                break;
+
+            case DriveToRow:
+                if(!_follower.isBusy()){
+                    ChangeState(DriveState.WaitForLoadTrigger);
+                }
+                break;
+
+            case WaitForLoadTrigger:
+                if (_loadTriggerPulled){
+                    switch (_targetSpecimenId){
+
+                        case AprilTagConstant.GPP:
+                            ApproachFirstRowForLoading();
+                            break;
+
+                        case AprilTagConstant.PGP:
+                            ApproachSecondRowForLoading();
+                            break;
+
+                        case AprilTagConstant.PPG:
+                            ApproachThirdRowForLoading();
+                            break;
+                    }
+
+                    ChangeState(DriveState.LoadingSpecimens);
+                    _loadTriggerPulled = false;
+                }
+                break;
+
+            case LoadingSpecimens:
+                if (!_follower.isBusy()){
+                    ChangeState(DriveState.LoadingComplete);
+                }
+                break;
+
+            case LoadingComplete:
+                if (_returnTriggerPulled){
+
+                    switch (_targetSpecimenId){
+
+                        case AprilTagConstant.GPP:
+                            ReturnFirstRowToLaunch();
+                            break;
+
+                        case AprilTagConstant.PGP:
+                            ReturnSecondRowToLaunch();
+                            break;
+
+                        case AprilTagConstant.PPG:
+                            ReturnThirdRowToLaunch();
+                            break;
+                    }
+
+                    ChangeState(DriveState.DriveToLaunch);
+                }
+
+            case DriveToLaunch:
+                if (!_follower.isBusy()){
+                    ChangeState(DriveState.ReadyToFire);
+                }
+                break;
+        }
+    }
+}
